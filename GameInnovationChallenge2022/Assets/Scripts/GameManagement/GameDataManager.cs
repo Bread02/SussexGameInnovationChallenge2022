56f116db70d865a3208b964940c42bb1f1475b81
Sylux02 <79101834+Bread02@users.noreply.github.com>
2022-02-15 17:06:13 +0000

    Added several changes to data manager for saving.

--- GameInnovationChallenge2022/Assets/Scripts/GameManagement/GameDataManager.cs
+++ GameInnovationChallenge2022/Assets/Scripts/GameManagement/GameDataManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 // keeps track of player inventory, points, skills, etc.
 public class GameDataManager : MonoBehaviour
 {
     public int skillPoints;
+    private int timesDied;
+    private GameDataManager gameDataManager;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
 
     // check whether player has these pet rock abilities unlocked
@@ -35,9 +44,74 @@ public class GameDataManager : MonoBehaviour
     public bool queenBee;
 
     // check which weapon is selected
-    public bool PetRockSelected;
-    public bool QuackAttackSelected;
+    private bool petRockSelected;
+    private bool platypusBeamSelected;
+    private bool quackAttackSelected;
+    private bool beesSelected;
 
+
+    public GameDataManager(int skillPoints, int timesDied, bool petRock,
+        bool shatterShot, bool volcanic, bool lava)
+    {
+        this.skillPoints = skillPoints;
+        this.timesDied = timesDied;
+        this.petRock = petRock;
+        this.shatterShot = shatterShot;
+        this.volcanic = volcanic;
+        this.lava = lava;
+        return;
+    }
+
+    private void ReadFromSave()
+    {
+        //Load achievments
+        string filePath = $"{Application.persistentDataPath}/Save/gameData.save";
+        if (File.Exists(filePath))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+
+            string json = (string)formatter.Deserialize(stream);
+            gameDataManager = JsonUtility.FromJson<GameDataManager>(json);
+
+            stream.Close();
+        }
+        else
+        {
+            Debug.LogWarning($"gameData File not found");
+          //  gameDataManager = new GameDataManager();
+        }
+    }
+
+    private void WriteToSave()
+    {
+        Debug.Log("Writing Player Save");
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        /*CreateSaveDirectoryPaths();*/
+        string filePath = $"{Application.persistentDataPath}/Save/gameData.save";
+        FileStream stream = new FileStream(filePath, FileMode.Create);
+        string json = JsonUtility.ToJson(gameDataManager);
+        formatter.Serialize(stream, json);
+        stream.Close();
+        Debug.Log("Saved gameData.");
+
+        /*
+        filePath = $"{Application.persistentDataPath}/Save/endings.save";
+        stream = new FileStream(filePath, FileMode.Create);
+        string json2 = JsonUtility.ToJson(endingsUnlocked);
+        formatter.Serialize(stream, json2);
+        stream.Close();
+        Debug.Log("Saved End.");
+        */
+    }
+
+
+    public void SaveGame()
+    {
+        WriteToSave();
+        Debug.Log("Save complete");
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +126,37 @@ public class GameDataManager : MonoBehaviour
     }
 
     // getter and setters
+
+    // finds the selected weapon, returns a number between 1 to 4 corresponding to the weapon
+    public int SelectedWeapon()
+    {
+        if(petRockSelected == true)
+        {
+            return 0;
+        }
+        if (platypusBeamSelected == true)
+        {
+            return 1;
+        }
+        if (quackAttackSelected == true)
+        {
+            return 2;
+        }
+        if (beesSelected == true)
+        {
+            return 3;
+        }
+        else
+        {
+            Debug.LogError("Weapon has not been selected!");
+            return -1;
+        }
+    }
+
+    public void DeathCount()
+    {
+        timesDied += 1;
+    }
     
 
 
--- GameInnovationChallenge2022/Assets/Scripts/GameManagement/PlayerAbilityCheck.cs
+++ GameInnovationChallenge2022/Assets/Scripts/GameManagement/PlayerAbilityCheck.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class PlayerAbilityCheck : MonoBehaviour
 {
 
-   // [SerializeField] private GameDataManager gameDataManager;
-    [SerializeField] private SkillTreeMaster skillTreeMaster;
+    [SerializeField] private GameDataManager gameDataManager;
+
 
     //weapon scripts
     [SerializeField] private PlayerBeamAttack playerBeamAttack;
@@ -19,18 +19,31 @@ public class PlayerAbilityCheck : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(skillTreeMaster.BoolPetRockSelected())
+        playerProjectileGun.enabled = false;
+        playerGooseAttack.enabled = false;
+        playerBeeSwarmAttack.enabled = false;
+        playerBeamAttack.enabled = false;
+        SelectedWeapon();
+    }
+
+    void SelectedWeapon()
+    {
+        if(gameDataManager.SelectedWeapon() == 0)
         {
             playerProjectileGun.enabled = true;
         }
-
-        if(skillTreeMaster.BoolQuackAttackSelected())
+        if (gameDataManager.SelectedWeapon() == 1)
+        {
+            playerBeamAttack.enabled = true;
+        }
+        if (gameDataManager.SelectedWeapon() == 2)
         {
             playerGooseAttack.enabled = true;
         }
-
-
-
+        if(gameDataManager.SelectedWeapon() == 3)
+        {
+            playerBeeSwarmAttack.enabled = true;
+        }
     }
 
     // Update is called once per frame
