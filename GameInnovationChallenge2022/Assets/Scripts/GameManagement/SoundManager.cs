9e7f8f0315f5c160c4b8756a5a881d212ba7e09e
Hcay <socyache@gmail>
2022-02-17 02:25:28 +0000

    MaxImplemented pause menu + sound manager for SFX
    Not yet working well, still in progress

---
+++ GameInnovationChallenge2022/Assets/Scripts/GameManagement/SoundManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource _musicSource, _effectsSource;
+
+    private void Awake()
+    {
+        if (Instance = null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        _effectsSource.PlayOneShot(clip);   
+    }
+
+    //Code Monkey's SoundManager Tutorial
+    /*public static void PlaySound()
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+    }*/
+
+}
