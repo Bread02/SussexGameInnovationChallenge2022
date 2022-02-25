9ec8538d0b3285388bdc834f02a3fa1b47ff1b0f
Sylux02 <79101834+Bread02@users.noreply.github.com>
2022-02-25 21:34:13 +0000

    Changes

--- GameInnovationChallenge2022/Assets/Scripts/Player/PlayerWeapons/PlayerBeeSwarmAttack.cs
+++ GameInnovationChallenge2022/Assets/Scripts/Player/PlayerWeapons/PlayerBeeSwarmAttack.cs
@@ -8,16 +8,41 @@ public class PlayerBeeSwarmAttack : MonoBehaviour
 {
     private GameObject player;
     [SerializeField] private GameObject beeSwarm;
+    [SerializeField] private bool fire;
+    [SerializeField] private PlayerActionControls playerActionControls;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         player = this.gameObject;
+        playerActionControls = new PlayerActionControls();
+
+        playerActionControls.PlayerControls.PlayerFireWeapon.performed += ctx => fire = true;
+        playerActionControls.PlayerControls.PlayerFireWeapon.canceled += ctx => fire = false;
+        beeSwarm.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FindEnemy()
+    {
+
+    }
+
+    void AttackEnemy()
+    {
+
+    }
+
+    void MoveTowardEnemy()
+    {
+
     }
 }
