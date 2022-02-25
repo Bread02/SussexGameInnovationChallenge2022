1103d83b97604176e98cf616682de353f31a9318
Sylux02 <79101834+Bread02@users.noreply.github.com>
2022-02-25 22:11:00 +0000

    Testing Sprite Pallets and code changes to bee attack

--- GameInnovationChallenge2022/Assets/Scripts/Player/PlayerWeapons/PlayerBeeSwarmAttack.cs
+++ GameInnovationChallenge2022/Assets/Scripts/Player/PlayerWeapons/PlayerBeeSwarmAttack.cs
@@ -10,9 +10,10 @@ public class PlayerBeeSwarmAttack : MonoBehaviour
     [SerializeField] private GameObject beeSwarm;
     [SerializeField] private bool fire;
     [SerializeField] private PlayerActionControls playerActionControls;
-
-
-
+    [SerializeField] private List<Transform> damageableGameObjects;
+    [SerializeField] private Vector2 currentPosition;
+    [SerializeField] private float minDist;
+    [SerializeField] private Transform tMin;
 
     // Start is called before the first frame update
     void Start()
@@ -28,21 +29,39 @@ public class PlayerBeeSwarmAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FindEnemy();
+
+        if(fire == true)
+        {
+            AttackEnemy();
+            MoveTowardEnemy();
+        }
     }
 
     void FindEnemy()
     {
-
+        foreach (GameObject foo in GameObject.FindGameObjectsWithTag("EnemyOne"))
+        {
+            damageableGameObjects.Add(foo.transform);
+        }
     }
 
-    void AttackEnemy()
+    Transform AttackEnemy()
     {
-
+        foreach (Transform t in damageableGameObjects)
+        {
+            float dist = Vector2.Distance(t.position, currentPosition);
+            if (dist < minDist)
+            {
+                tMin = t;
+                minDist = dist;
+            }
+        }
+        return tMin;
     }
 
     void MoveTowardEnemy()
     {
-
+        beeSwarm.transform.position = Vector2.MoveTowards(transform.position, tMin.transform.position, 2);
     }
 }
